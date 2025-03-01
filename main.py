cb0aa071e5516b524b9da66a2a91a3eed600f152
Raptar_Original <nickrappe@gmail.com>
2025-02-28 19:36:52 -0600

    refactored my solution to match lesson solution

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from stats import get_num_words
-from stats import get_character_counts
+from stats import get_num_words, get_chars_dict
+
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    num_characters = get_character_counts(text)
+    chars_dict = get_chars_dict(text)
     print(f'{num_words} words found in the document')
-    print(num_characters)
+    print(chars_dict)
 
 
 def get_book_text(path):
--- stats.py
+++ stats.py
@@ -2,13 +2,13 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-def get_character_counts(text):
-    lowercase_text = text.lower()
-    character_count = {}
-    characters = list(lowercase_text)
-    for char in characters:
-        if char not in character_count:
-            character_count[char] = 1
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
         else:
-            character_count[char] += 1
-    return character_count
+            chars[lowered] = 1
+    return chars
